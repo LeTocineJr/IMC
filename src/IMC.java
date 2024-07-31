efa093627a54f5f3e5c830bf30b4cc8e84b91d7e
LeTocineJr-Nitro5 <ivancitorojas.c@gmail.com>
2024-07-31 13:15:39 -0600

    Se agregan comentarios

--- src/IMC.java
+++ src/IMC.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class IMC {
     public static void main(String[] args) {
+        //Se crea la clase escaner
         Scanner scanner = new Scanner(System.in);
 
         double altura, peso, imc;
+        //SOlicita los datos al usuario
         System.out.println("Ingresa tu Altura ");
         altura = scanner.nextDouble();
         System.out.println("Ingresa tu Peso ");
         peso = scanner.nextDouble();
-
+        //Formula de IMC
         imc = peso / (Math.pow(altura,2));
+        //Imprime resultado
         System.out.println("Tu IMC es: " + imc);
     }
 }
