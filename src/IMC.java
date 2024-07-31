e06879e58cc40629a895ad222655a3b07c21d536
LeTocineJr-Nitro5 <ivancitorojas.c@gmail.com>
2024-07-31 13:55:11 -0600

    Modifica lineas de codigo, para impresion en terminal por impresion de ventanas

--- src/IMC.java
+++ src/IMC.java
@@ -1,19 +1,17 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class IMC {
     public static void main(String[] args) {
-        //Se crea la clase escaner
-        Scanner scanner = new Scanner(System.in);
 
         double altura, peso, imc;
-        //SOlicita los datos al usuario
-        System.out.println("Ingresa tu Altura ");
-        altura = scanner.nextDouble();
-        System.out.println("Ingresa tu Peso ");
-        peso = scanner.nextDouble();
+        //Solicita los datos al usuario
+        double height = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa tu Altura"));
+        double weight = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa tu Peso"));
+
         //Formula de IMC
-        imc = peso / (Math.pow(altura,2));
+        imc = weight / (Math.pow(height,2));
         //Imprime resultado
-        System.out.println("Tu IMC es: " + imc);
+        JOptionPane.showMessageDialog(null,"Tu IMC es: " + imc);
     }
 }
