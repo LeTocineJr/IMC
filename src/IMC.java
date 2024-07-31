8e8e2eb56dbc5aec6402629918a482540c075c69
LeTocineJr-Nitro5 <ivancitorojas.c@gmail.com>
2024-07-31 14:14:21 -0600

    Se crea la clase DecimalFormat, se formatea el resultado

--- src/IMC.java
+++ src/IMC.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
-import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class IMC {
     public static void main(String[] args) {
-
-        double altura, peso, imc;
+        //Creamos la clase DecimalFormat
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
         //Solicita los datos al usuario
         double height = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa tu Altura"));
         double weight = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa tu Peso"));
 
         //Formula de IMC
-        imc = weight / (Math.pow(height,2));
+        double IMC = weight / (Math.pow(height,2));
         //Imprime resultado
-        JOptionPane.showMessageDialog(null,"Tu IMC es: " + imc);
+        JOptionPane.showMessageDialog(null,"Tu IMC es: " + decimalFormat.format(IMC));
     }
 }
